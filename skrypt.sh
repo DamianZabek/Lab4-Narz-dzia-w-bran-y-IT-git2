b24e0fc9936ad9aca55950dac9bab82c4588f5dc
DamianZabek <damianzabek132@gmail.com>
2026-05-30 12:01:46 +0200

    Zoptymalizowano --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,8 @@
 if [["$1"=="--date"]]; then
 	date
 elif [["$1"=="--logs"]]; then
-	for i in  {1..100}; do
+	count=${2:-100}
+	for ((i=1; i<=count; i++)); then
 		echo -e "Nazwa pliku: log${i}.txt\nNazwa skryptu: $0\nData: $(date)">"log${i}.txt"
 	done
 fi
