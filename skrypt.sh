14120535dcbc974ee9084873fe5b46f55b4e51f5
DamianZabek <damianzabek132@gmail.com>
2026-05-30 12:04:50 +0200

    Dodano flage --help

--- skrypt.sh
+++ skrypt.sh
@@ -6,4 +6,9 @@ elif [["$1"=="--logs"]]; then
 	for ((i=1; i<=count; i++)); then
 		echo -e "Nazwa pliku: log${i}.txt\nNazwa skryptu: $0\nData: $(date)">"log${i}.txt"
 	done
+elif [["$1"=="--help"]] then
+	echo "Dostepne opcje:"
+	echo "--date    Wyswietla dzisiejsza date"
+	echo "--logs [x]   tworzy x plikow log"
+	echo "--help      Wyswietla liste z pomoca"
 fi
