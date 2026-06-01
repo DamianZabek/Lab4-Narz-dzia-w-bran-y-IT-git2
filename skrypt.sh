3885c5aa67a85df9d0ddaa20559f8b920f539580
DamianZabek <damianzabek132@gmail.com>
2026-06-01 08:40:38 +0200

    Dodano krotkie flagi

--- skrypt.sh
+++ skrypt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if [["$1"=="--date"]]; then
-	date
-elif [["$1"=="--logs"]]; then
-	count=${2:-100}
-	for ((i=1; i<=count; i++)); then
-		echo -e "Nazwa pliku: log${i}.txt\nNazwa skryptu: $0\nData: $(date)">"log${i}.txt"
-	done
-elif [["$1"=="--help"]] then
-	echo "Dostepne opcje:"
-	echo "--date    Wyswietla dzisiejsza date"
-	echo "--logs [x]   tworzy x plikow log"
-	echo "--help      Wyswietla liste z pomoca"
+if [[ "$1" == "--date" ]] || [[ "$1" == "-d" ]]; then
+    date
+elif [[ "$1" == "--logs" ]] || [[ "$1" == "-l" ]]; then
+    count=${2:-100}
+    for ((i=1; i<=count; i++)); then
+        echo -e "Nazwa pliku: log${i}.txt\nNazwa skryptu: $0\nData: $(date)">"log${i}.txt"
+    done
+elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+    echo "Dostepne opcje:"
+    echo "--date    Wyswietla dzisiejsza date"
+    echo "--logs [x]   tworzy x plikow log"
+    echo "--help      Wyswietla liste z pomoca"
 fi
